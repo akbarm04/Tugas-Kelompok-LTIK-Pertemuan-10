719031a972067804707f83286153112944b0dfad
akbarm04 <am8343969@gmail.com>
2025-11-06 09:27:26 +0700

    edit file and edit feature

---
+++ kolaborasi.py
@@ -0,0 +1,38 @@
+# perkenalan.py - File Awal
+
+# Kontribusi Anggota 1 (Akbar)
+def perkenalan_akbar():
+    print("- Anggota 1 (Akbar): [Saya Akbar Maulana sebagai Project Manager disini....]")
+
+# Kontribusi Anggota 2 (namakalian2)
+def perkenalan_namakalian2():
+    print("- Anggota 2 (namakalian2): [Masukkan deskripsi unik namakalian2 di sini]")
+
+# Kontribusi Anggota 3 (namakalian3)
+def perkenalan_namakalian3():
+    print("- Anggota 3 (namakalian3): [Masukkan deskripsi unik namakalian3 di sini]")
+
+# Kontribusi Anggota 4 (namakalian4)
+def perkenalan_namakalian4():
+    print("- Anggota 4 (namakalian4): [Masukkan deskripsi unik Mira di sini]")
+
+# Kontribusi Anggota 5 (namakalian5)
+def perkenalan_namakalian5():
+    print("- Anggota 5 (namakalian5): [Masukkan deskripsi unik namakalian5 di sini]")
+
+
+
+def main():
+    print("--- Proyek Kolaborasi Kelompok RLIIS ---")
+    print("Anggota yang berkontribusi:")
+
+    # Panggil semua fungsi di sini
+    perkenalan_akbar()
+    perkenalan_namakalian2()
+    perkenalan_namakalian3()
+    perkenalan_namakalian4()
+    perkenalan_namakalian5()
+
+
+if __name__ == "__main__":
+    main()
--- tes.py
+++
@@ -1,8 +0,0 @@
-print("MIRA APPS")
-
-print("Akbar Maulana")
-print("Anggota Kelompok 2")
-print("Anggota Kelompok 3")
-print("Anggota Kelompok 4")
-print("Anggota Kelompok 5")
-
